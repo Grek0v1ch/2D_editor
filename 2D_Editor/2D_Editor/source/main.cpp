3ee71d8c8287672ad1cf72eb59ea54c5e3f02b61
yrlttlsstr <achesdox@gmail.com>
2022-05-05 21:26:02 +0300

    Написан класс треугольника.

--- 2D_Editor/2D_Editor/source/main.cpp
+++ 2D_Editor/2D_Editor/source/main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
 #include "../headers/point.hpp"
 #include "../headers/segment.hpp"
+#include "../headers/triangle.hpp"
 
 int main() {
 	Segment s(Point(0, 0), Point(5, 5));
 	std::cout << s;
+
+	Triangle t(Point(0, 0), Point(5, 5), Point(2, 2));
+	Triangle f(Point(1, 3), Point(5, 5), Point(0, 2));
+	std::cout << t << f;
+	std::cout << (t == f) << '\n';
+	f = t;
+	std::cout << f << '\n';
+
 	return 0;
 }
---
+++ 2D_Editor/2D_Editor/source/triangle.cpp
@@ -0,0 +1,40 @@
+#include "../headers/triangle.hpp"
+
+// Конструкторы:
+Triangle::Triangle() {
+	for (int i = 0; i < 3; i++)
+		_vertices[i] = Point(0, 0);
+}
+Triangle::Triangle(Point point1, Point point2, Point point3) {
+		_vertices[0] = point1;
+		_vertices[1] = point2;
+		_vertices[2] = point3;
+}
+// Копирования.
+Triangle::Triangle(const Triangle& object) {
+	for (int i = 0; i < 3; i++)
+		_vertices[i] = object._vertices[i];
+}
+// Деструктор.
+Triangle::~Triangle() {
+	for (int i = 0; i < 3; i++)
+		_vertices[i] = Point(0, 0);
+}
+// Переопределение оператора присваивания.
+Triangle& Triangle::operator=(const Triangle& value) {
+	if (&value == this) return *this;
+	for (int i = 0; i < 3; i++)
+		_vertices[i] = value._vertices[i];
+	return *this;
+}
+// Переопределение оператора сравнения (оператор проверяет идентичность двух объектов класса).
+bool Triangle::operator==(const Triangle& object) {
+	for (int i = 0; i < 3; i++)
+		if (!(_vertices[i] == object._vertices[i])) return false;
+	return true;
+}
+// Переопределение оператора вывода.
+std::ostream& operator<<(std::ostream& fout, const Triangle& object) {
+	fout << "Triangle, vertices: " << '\n' << object._vertices[0] << object._vertices[1] << object._vertices[2] << '\n';
+	return fout;
+}
