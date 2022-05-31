00d2bbc1e94946e8318d01641701e5fb8821bc84
Ivan Voronov <ivvoronov2308@gmail.com>
2022-05-31 19:54:11 +0300

    Настройка окна приложения GLUT. Теперь центр координат расположен в центре окна

--- src/main.cpp
+++ src/main.cpp
@@ -7,37 +7,25 @@
 #include "data_structures/dllist.hpp"
 
 #ifdef __APPLE__
-#define GL_SILENCE_DEPRECATION
-#include <OpenGL/gl.h>
-#include <GLUT/glut.h>
+    #define GL_SILENCE_DEPRECATION
+    #include <OpenGL/gl.h>
+    #include <GLUT/glut.h>
 #endif
 #ifdef __linux__
-#include <GL/glut.h>
+    #include <GL/glut.h>
 #endif
 #ifdef _WIN32
-#include "../build/glut-3.7.6-bin/glut.h"
+    #include "../build/glut-3.7.6-bin/glut.h"
 #endif
 
 /* начальная ширина и высота окна */
 GLint Width = 512, Height = 512;
-/* размер куба */
-const int CubeSize = 200;
-/* эта функция управляет всем выводом на экран */
+
 void Display(void) {
-    int left, right, top, bottom;
-    left = (Width - CubeSize) / 2;
-    right = left + CubeSize;
-    bottom = (Height - CubeSize) / 2;
-    top = bottom + CubeSize;
     glClearColor(0, 0, 0, 1);
     glClear(GL_COLOR_BUFFER_BIT);
     glColor3ub(255, 0, 0);
-    glBegin(GL_QUADS);
-    glVertex2f(left, bottom);
-    glVertex2f(left, top);
-    glVertex2f(right, top);
-    glVertex2f(right, bottom);
-    glEnd();
+
     glFinish();
 }
 
@@ -51,7 +39,9 @@ void Reshape(GLint w, GLint h) {
     /* ортографическая проекция */
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    glOrtho(0, w, 0, h, -1.0, 1.0);
+    // Команда устанавливает центр координат в центре отоброжаемого окна. Также масштаб координат
+    // теперь приравнивается к масштабу пикселей.
+    gluOrtho2D(-w / 2, w / 2, -h / 2, h / 2);
 
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
@@ -59,10 +49,9 @@ void Reshape(GLint w, GLint h) {
 
 /* Функция обрабатывает сообщения от клавиатуры */
 void Keyboard(unsigned char key, int x, int y) {
-#define ESCAPE '\033'
+    #define ESCAPE '\033'
 
-    if (key == ESCAPE)
-        exit(0);
+    if (key == ESCAPE) exit(0);
 }
 
 /* Главный цикл приложения */
@@ -70,7 +59,7 @@ int main(int argc, char* argv[]) {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_RGB);
     glutInitWindowSize(Width, Height);
-    glutCreateWindow("Red square example");
+    glutCreateWindow("2D editor");
     glutDisplayFunc(Display);
     glutReshapeFunc(Reshape);
     glutKeyboardFunc(Keyboard);
