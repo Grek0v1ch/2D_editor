b831474773cb32d90909d063e9eb32e565987c74
Oycea <margaret8958100@gmail.com>
2022-06-03 12:54:32 +0300

    Добавлены исходные файлы для проверки ввода

---
+++ src/data_check/data_check.hpp
@@ -0,0 +1,12 @@
+#ifndef DATA_CHECK_HPP
+#define DATA_CHECK_HPP
+
+#include <iostream>
+
+//корректный ввод натурального числа
+void check_value(int& num);
+
+//корректный ввод вещественного числа
+void input_menu_command(int& command);
+
+#endif //DATA_CHECK_HPP
