b12b8f649d2145135d7c26bfb388afd9b20bdff3
Ivan Voronov <ivvoronov2308@gmail.com>
2022-06-07 18:47:48 +0300

    Добавлен функционал по удалению/возвращению элементов с экрана

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include "quadrangle/quadrangle.hpp"
 #include "ellipse/ellipse.hpp"
 #include "data_structures/dllist.hpp"
+#include "data_structures/stack.hpp"
 #include "menu/menu.hpp"
 
 #ifdef __APPLE__
@@ -22,6 +23,9 @@
 /* начальная ширина и высота окна */
 GLint Width = 640, Height = 640;
 
+Stack<Ishape*> _buff_;
+
+// Функция рисует все фигуры, которые есть в списке.
 void draw_objects() {
 	auto iter = _objects_.front();
 	while (iter) {
@@ -30,6 +34,7 @@ void draw_objects() {
 	}
 }
 
+// Функция чистит список фигур.
 void clear_objects() {
 	auto iter = _objects_.front();
 	while (iter) {
@@ -39,8 +44,46 @@ void clear_objects() {
 	}
 }
 
+// Функция чистит буфер фигур.
+void clear_buff() {
+	Ishape* item;
+	while (true) {
+		// Сигналом того, что буфер пуст служит ошибка в методе back().
+		try {
+			item = _buff_.back();
+		} catch (...) {
+			break;
+		}
+		delete item;
+		_buff_.pop();
+	}
+}
+
+// Функция удаляет объект с экрана.
+void del_object() {
+	if (_objects_.is_empty()) return;
+	auto item = _objects_.back();
+	_buff_.push(item->value());
+	_objects_.remove_last();
+}
+
+// Функция возвращает удаленный объект на экран.
+void return_object() {
+	Ishape* item;
+	// Сигналом того, что буфер пуст служит ошибка в методе back().
+	try {
+		item = _buff_.back();
+	} catch (...) {
+		return;
+	}
+	_objects_.push_back(item);
+	_buff_.pop();
+}
+
+// Функция устанавливает белый цвет фона и рисует черную координатную сетку.
 void draw_main_window() {
 	glColor3ub(0, 0, 0);
+	// Сначала рисуем все вертикальные полосы, по две за итерацию цикла.
 	for (int i = 0; i < Width; i += 20) {
 		if (i == 0)
 			glLineWidth(2);
@@ -57,7 +100,7 @@ void draw_main_window() {
 			glVertex2f(-i, -Height);
 		glEnd();
 	}
-
+	// Потом рисуем все горизонтальные полосы, по две за итерацию цикла.
 	for (int i = 0; i < Height; i += 20) {
 		if (i == 0)
 			glLineWidth(2);
@@ -91,7 +134,7 @@ void Display(void) {
 // Эта функция перерисовывает картинку через опеределенный интервал времени (в этой программе 33
 // милисекунды). В ней просто дублируется часть функции Display, отвечающей за отрисовку.
 void TimerMove(int value) {
-	// draw_main_window();
+	draw_main_window();
 	if (_MENU_) main_menu();
 	draw_objects();
 
@@ -120,18 +163,19 @@ void Reshape(GLint w, GLint h) {
 /* Функция обрабатывает сообщения от клавиатуры */
 void Keyboard(unsigned char key, int x, int y) {
 	#define ESCAPE '\033'
-
+	// Все клавиши необхожимо нажимать с включенной английской раскладкой.
+	// Если нажата клавиша esc окно OpenGL закрывается и программа завершается.
 	if (key == ESCAPE) {
 		clear_objects();
+		clear_buff();
 		exit(0);
 	}
+	// Если нажата клавиша m происходит переключение с окна OpenGL на меню.
 	if (key == 109) _MENU_ = true;
-	if (key == 122) {
-		if (_objects_.is_empty()) return;
-		auto item = _objects_.back();
-		delete item->value();
-		_objects_.remove_last();
-	}
+	// Если нажата клавиша z то объект удаляется.
+	if (key == 122) del_object();
+	// Если нажата клавиша x то объект возвращается.
+	if (key == 120) return_object();
 }
 
 /* Главный цикл приложения */
